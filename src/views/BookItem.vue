4e2861e1b27fc5769f01d45413203a5ab5772907
ididid8 <lihui920808@163.com>
2019-08-12 09:11:10 +0800

    page

---
+++ src/views/BookItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="movie-view has-header">
+    <scorller title="最受关注图书 | 虚构类" type="hasCover" :items="novel"></scorller>
+    <scorller title="最受关注图书 | 非虚构类" type="hasCover" :items="reality"></scorller>
+    <scorller title="豆瓣纸书" type="hasCover" :items="travel">
+      
+    </scorller>
+    
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+import Scorller from '../components/Scroller'
+import Types from '../components/Types'
+import DownloadApp from '../components/DownloadApp'
+
+export default {
+  name: 'book-view',
+  components: {Scorller, Types, DownloadApp},
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState({
+      novel: state => state.book.novel,
+      reality: state => state.book.reality,
+      travel: state => state.book.travel,
+      bookTags: state => state.book.bookTags
+    })
+  },
+  methods: {
+    getBook: function () {
+      this.$store.dispatch('getBook')
+    }
+  },
+  created () {
+    this.getBook()
+  }
+}
+
+</script>
+
+<style>
+</style>
