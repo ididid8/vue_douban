1d023617002e5a552bf8bbe81b1c7fdf675df283
lihui41 <lihui920808@163.com>
2019-08-13 21:00:16 +0800

    pages

--- src/views/TalionView.vue
+++ src/views/TalionView.vue
@@ -6,10 +6,51 @@
         <input
          type="search"
          name="query"
-         
-         />
+         v-model.trim.lazy="queryStr"
+         @keyup.enter="goSearch()">
       </form>
     </div>
+    <ul class="has-header">
+      <li>
+        <div>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(35, 132, 232);">电影</strong>
+            <span>影院热映</span>
+          </a>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(230, 70, 126);">同城</strong>
+            <span>周末活动</span>
+          </a><a href="#" target="_blank">
+            <strong style="color: rgb(159, 120, 96);">阅读</strong>
+            <span>电子书</span>
+          </a>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(225, 100, 77);">东西</strong>
+            <span>恋爱之物</span>
+          </a>
+        </div>
+      </li>
+      <li>
+        <div>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(122, 105, 219);">电视</strong>
+            <span>正在热播</span>
+          </a>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(42, 184, 204);">小组</strong>
+            <span>志趣相投</span>
+          </a>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(87, 116, 197);">游戏</strong>
+            <span>虚拟世界</span>
+          </a>
+          <a href="#" target="_blank">
+            <strong style="color: rgb(64, 207, 169);">FM</strong>
+            <span>红心歌单</span>
+          </a>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
